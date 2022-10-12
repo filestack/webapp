6e8cf2d69ad39ea06c4ee72db2c3e9fc2cc62691
Bharath Kandukoori <93364948+Bharath-Kandukoori@users.noreply.github.com>
2022-10-12 01:47:37 -0700

    Update upload-results.py

--- upload-results.py
+++ upload-results.py
@@ -14,7 +14,7 @@ def upload_results(host, user, api_key, scanner, result_file, engagement_id, ver
 	files = dict()
 
 	# Prepare headers
-	# headers = {'Authorization': 'Token b4e1b9a5f80cc8d96363b515f039170c2aa222db'}
+	# headers = {'Authorization': 'ApiKey dojo:b4e1b9a5f80cc8d96363b515f039170c2aa222db'}
 	headers['Authorization'] = AUTH_TOKEN
 	print headers
 
