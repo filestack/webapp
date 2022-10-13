669ddf70169b5a02aa6b6b770f3b4db8bcce1c95
Bharath Kandukoori <93364948+Bharath-Kandukoori@users.noreply.github.com>
2022-10-13 04:29:06 -0700

    Update upload-results.py

--- upload-results.py
+++ upload-results.py
@@ -44,8 +44,8 @@ def upload_results(host, user, api_key, scanner, result_file, engagement_id, ver
 	# Prepare file data to send to API
 	files['file'] = open(result_file)
 
-    # Make a request to API
-    response = requests.post(IMPORT_SCAN_URL, headers=headers, files=files, data=json, verify=verify)
+		# Make request to API
+	response = requests.post(IMPORT_SCAN_URL, headers=headers, files=files, data=json, verify=verify)
     # print r.request.body
     # print r.request.headers
     # print r.status_code
